b332f5e0c327ea5c6e04d72518a706aa3ef07ba3
gabriel <gabrielvettorazzii@gmail.com>
2026-02-23 19:22:05 -0300

    fix functions

--- supabase/functions/send-email/index.ts
+++ supabase/functions/send-email/index.ts
@@ -1,6 +1,6 @@
 const corsHeaders = {
     'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+    'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type, x-user-id',
     'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
 }
 
